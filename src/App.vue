edd7731abba1f262836f11f9990aef8ca747a72e
nquidox <nquidox@gmail.com>
2024-09-17 21:56:17 +0300

    Navbar + Home page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
+import {defineComponent} from "vue";
+import Navbar from "@/components/Navbar.vue";
+
+export default defineComponent({
+  components: {Navbar}
+})
+
 </script>
 
 <template>
+  <Navbar />
   <RouterView />
 </template>
 
