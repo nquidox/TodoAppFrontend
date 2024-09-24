d9aa6cadd99dfa41fed3362b2e26c22ef082bb6a
nquidox <nquidox@gmail.com>
2024-09-24 23:06:34 +0300

    todo lists moved to component, tasks crud added

---
+++ src/components/TodoList.vue
@@ -0,0 +1,166 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "TodoList",
+
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    }
+  },
+
+  data(){
+    return {
+      taskEdit: false,
+
+      list: this.item,
+
+      task:{},
+      tasks: [],
+    }
+  },
+
+  methods: {
+    updateList(){
+      this.$emit('update-list', this.item.id);
+    },
+
+    deleteList() {
+      this.$emit('delete-list', this.item.id);
+    },
+
+    editList(list){
+      list.isEdit = true;
+      list.newTitle = list.title;
+    },
+
+    saveList(list){
+      list.isEdit = false;
+      list.title = list.newTitle;
+      this.updateList();
+    },
+
+    async createTaskReq(listId, task){
+      await axios.post(`/todo-lists/${listId}/tasks`, {"title": task.title});
+    },
+
+    async getTasksReq(listId){
+      await axios.get(`/todo-lists/${listId}/tasks`).then(response => this.tasks = response.data);
+    },
+
+    async updateTaskReq(listId, task){
+      await axios.put(`/todo-lists/${listId}/tasks/${task.id}`, {"title": task.title});
+    },
+
+    async deleteTaskReq(listId, task){
+      await axios.delete(`/todo-lists/${listId}/tasks/${task.id}`);
+    },
+
+    async createTask(task){
+      await this.createTaskReq(this.list.id, task);
+      await this.getTasksReq(this.list.id);
+      this.taskEdit = false;
+      this.task = {};
+    },
+
+    async getTasks(){
+      await this.getTasksReq(this.list.id);
+    },
+
+    async updateTask(task){
+      await this.updateTaskReq(this.list.id, task);
+    },
+
+    async deleteTask(task){
+      await this.deleteTaskReq(this.list.id, task);
+      await this.getTasksReq(this.list.id);
+      this.task = {}
+    },
+
+    editTask(task){
+      task.isEdit = true;
+    },
+    saveTask(task){
+      this.updateTask(task)
+      task.isEdit = false;
+    },
+
+  },
+
+  created() {
+    this.getTasks()
+  },
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="card mt-3">
+      <div class="card-header card-header-green d-flex justify-content-between">
+        <div v-if="!list.isEdit">
+          <span> {{ list.title }} </span>
+        </div>
+        <div v-else>
+          <input class="border-black border-2" v-model="list.newTitle" placeholder="list.title" />
+          <img src="../assets/icons/check.svg" class="hover-image bordered m-1" @click="saveList(list)" alt="save" height="24" width="24" />
+          <img src="../assets/icons/cancel.svg" class="hover-image m-1" @click="list.isEdit = !list.isEdit" alt="cancel" height="24" width="24" />
+        </div>
+        <div>
+          <img src="../assets/icons/edit.svg" class="hover-image m-1" @click="editList(list)" alt="edit" height="24" width="24" />
+          <img src="../assets/icons/trash.svg" class="hover-image m-1" @click="deleteList(list)" alt="delete" height="24" width="24" />
+        </div>
+
+      </div>
+      <ol class="list-group list-group-flush">
+        <li class="list-group-item" v-for="task in tasks" :key="task">
+          <span class="fs-6" v-if="!task.isEdit">
+            <img src="../assets/icons/circle.svg" class="m-1" alt="*" height="24" width="24" />
+            {{ task.title }}
+          </span>
+          <span v-else>
+            <img src="../assets/icons/circle.svg" class="m-1" alt="*" height="24" width="24" />
+            <input class="border-black border-2" v-model="task.title" placeholder="Task title" />
+            <img src="../assets/icons/check.svg" class="hover-image bordered m-1" @click="saveTask(task)" alt="save" height="24" width="24" />
+            <img src="../assets/icons/cancel.svg" class="hover-image m-1" @click="task.isEdit = !task.isEdit" alt="cancel" height="24" width="24" />
+          </span>
+          <img src="../assets/icons/edit.svg" class="hover-image m-1" @click="editTask(task)" alt="edit" height="24" width="24" />
+          <img src="../assets/icons/trash.svg" class="hover-image m-1" @click="deleteTask(task)" alt="delete" height="24" width="24" />
+        </li>
+        <li class="list-group-item">
+          <div v-if="!taskEdit">
+            <span class="fs-6" @click="taskEdit = !taskEdit">
+              <img src="../assets/icons/plus.svg" class="hover-image m-1" alt="add task" height="24" width="24" />
+              Add task
+            </span>
+          </div>
+          <div v-else>
+            <input class="border-black border-2" v-model="task.title" placeholder="New task" />
+            <img src="../assets/icons/check.svg" class="hover-image bordered m-1" @click="createTask(task)" alt="create" height="24" width="24" />
+            <img src="../assets/icons/cancel.svg" class="hover-image m-1" @click="taskEdit = !taskEdit" alt="cancel" height="24" width="24" />
+          </div>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bordered{
+  border: 2px solid;
+  border-radius: 5px;
+}
+
+.card-header-green{
+  background-color: aquamarine;
+}
+
+.hover-image{
+  transition: 0.3s;
+}
+
+.hover-image:hover {
+  transform: scale(1.1);
+}
+</style>
--- src/views/TodoListsView.vue
+++ src/views/TodoListsView.vue
@@ -1,95 +1,73 @@
 <script>
 import axios from "axios";
+import TodoList from "@/components/TodoList.vue";
 
 export default {
   name: "TodoListsView",
+  components: {TodoList},
 
   data(){
     return {
       showCreateForm: false,
-      newList: {
-        title: "new title",
-        order: 0,
-      },
-      newTask: {
-        title: "new task",
-      },
-
+      newList: {},
       lists: [],
-      tasks:[],
     }
   },
 
   methods: {
-    createListReq(list){
-      axios.post(`/todo-lists`, {"title": list.title, "order": 0});
+    async createListReq(list){
+      await axios.post(`/todo-lists`, {"title": list.title, "order": 0});
     },
 
-    getAllLists(){
-      axios.get(`/todo-lists`).then(response => {
-        this.lists = response.data;
-        this.lists.forEach(list => {this.getListTasksReq(list.id);});
-      });
+    async getAllLists(){
+      await axios.get(`/todo-lists`).then(response => this.lists = response.data);
     },
 
-    updateListReq(list){
-      axios.put(`/todo-lists/${list.id}`, {"title": list.title, "order": 0});
+    async updateListReq(list){
+      await axios.put(`/todo-lists/${list.id}`, {"title": list.title, "order": 0});
     },
 
-    deleteListReq(listId){
-      axios.delete(`/todo-lists/${listId}`);
+    async deleteListReq(list){
+      await axios.delete(`/todo-lists/${list.id}`);
     },
 
-    getListTasksReq(id){
-      axios.get(`/todo-lists/${id}/tasks`).then(response => this.tasks.push({id: id, data: response.data}))
-    },
-
-    createList(list){
-      this.lists.unshift(list);
+    async createList(list){
+      await this.createListReq(list);
+      this.lists = [] //FIX ME
+      await this.getAllLists();
       this.showCreateForm = !this.showCreateForm;
-      this.createListReq(list);
+      this.newList = {};
     },
 
-    editList(list){
-      list.isEdit = true;
-      list.newTitle = list.title;
+    async readLists(){
+      await this.getAllLists();
     },
 
-    saveList(list){
-      list.isEdit = false;
-      list.title = list.newTitle;
-      this.updateListReq(list);
+    async updateList(list){
+      await this.updateListReq(list);
     },
 
-    deleteList(listId){
-      const index = this.lists.findIndex(list => list.id === listId);
-      if (index > -1) {
-        this.lists.splice(index, 1);
-      }
-      this.deleteListReq(listId);
-    },
-
-    getTasksByListId(listId) {
-      const listTasks = this.tasks.find(task => task.id === listId);
-      return listTasks ? listTasks.data : [];
+    async deleteList(list){
+      await this.deleteListReq(list);
+      this.lists = [] //FIX ME
+      await this.getAllLists();
     },
   },
 
   beforeMount(){
-    this.getAllLists();
+    this.readLists();
   },
 }
 </script>
 
 <template>
-  <div class="container text-center">
-    <div class="row mt-3">
-      <span class="fs-2" @click="showCreateForm = !showCreateForm">
+  <div class="container">
+    <div class="row d-flex mt-3">
+      <span class="fs-2 text-center" @click="showCreateForm = !showCreateForm">
         <img src="../assets/icons/new_todo.svg" class="hover-image m-1" alt="edit" height="40" width="40" />
         Create new todo list
       </span>
     </div>
-
     <div class="card mt-3" v-if="showCreateForm === true">
       <div class="card-header card-header-blue d-flex justify-content-center">
         <input class="border-black border-2" v-model="newList.title" placeholder="Enter todo list name" />
@@ -100,59 +78,23 @@ export default {
   </div>
 
   <div class="container">
-    <div class="card mt-3" v-for="list in lists" :key="list.id">
-      <div class="card-header card-header-green d-flex justify-content-between">
-        <div v-if="!list.isEdit">
-          <span>{{ list.title }}</span>
-        </div>
-        <div v-else>
-          <input class="border-black border-2" v-model="list.newTitle" placeholder="list.title" />
-          <img src="../assets/icons/check.svg" class="hover-image bordered m-1" @click="saveList(list)" alt="save" height="24" width="24" />
-        </div>
-        <div>
-          <img src="../assets/icons/edit.svg" class="hover-image m-1" @click="editList(list)" alt="edit" height="24" width="24" />
-          <img src="../assets/icons/trash.svg" class="hover-image m-1" @click="deleteList(list.id)" alt="delete" height="24" width="24" />
-        </div>
-
-      </div>
-      <ol class="list-group list-group-flush">
-        <li class="list-group-item" v-for="task in getTasksByListId(list.id)" :key="task.id">
-          <span class="fs-6">
-            <img src="../assets/icons/circle.svg" class="m-1" alt="*" height="24" width="24" />
-            {{ task.title }}
-          </span>
-        </li>
-        <li class="list-group-item">
-            <span class="fs-6">
-              <img src="../assets/icons/plus.svg" class="hover-image m-1" @click="" alt="add task" height="24" width="24" />
-              Add task
-            </span>
-        </li>
-      </ol>
-    </div>
+  <todo-list v-for="(list, index) in lists" :key="index" :item="list"
+             @update-list="updateList(list)"
+             @delete-list="deleteList(list)"
+  />
   </div>
 </template>
 
 <style scoped>
-
-.bordered{
-  border: 2px solid;
-  border-radius: 5px;
-}
-
 .card-header-blue{
   background-color: #34aaff;
-}
-
-.card-header-green{
-  background-color: aquamarine;
 }
 
 .hover-image{
   transition: 0.3s;
 }
 
-.hover-image:hover{
+.hover-image:hover {
   transform: scale(1.1);
 }
 </style>
