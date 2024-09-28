fa4e33f82763b19abb1c24ff7997fee4a642b0c5
nquidox <nquidox@gmail.com>
2024-09-28 16:41:53 +0300

    shrink-expand component

---
+++ src/components/shrinkExpand.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: "shrinkExpand",
+
+  props: {
+    item: {
+      type: Object,
+      required: true
+    },
+    expanded: {
+      type: Boolean,
+      required: true,
+    }
+  },
+
+  methods: {
+    expandSwitcher(){
+      this.$emit("expand-switcher", this.item.id);
+    },
+  },
+}
+</script>
+
+<template>
+  <div v-if="!expanded">
+    <img src="@/assets/icons/double-down.svg" class="svg-icon mx-1" @click="expandSwitcher" alt="expand" />
+  </div>
+  <div v-else>
+    <img src="@/assets/icons/double-up.svg" class="svg-icon mx-1" @click="expandSwitcher" alt="shrink" />
+  </div>
+</template>
+
+<style scoped>
+@import "@/assets/custom.css";
+</style>
